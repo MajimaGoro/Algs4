9ff612e36a31f799835491501714414e067fc5be
MajimaGoro <sxz7@qq.com>
2017-07-31 22:16:32 +0800

    Ex1.1.33

--- Chapter_01/Ex_1_1_33.java
+++ Chapter_01/Ex_1_1_33.java
@@ -25,6 +25,46 @@ public class Ex_1_1_33 {
     //向量点乘
     //https://baike.baidu.com/item/%E5%B9%B3%E9%9D%A2%E5%90%91%E9%87%8F/448934?fr=aladdin
     public static double dot(double[] x, double[] y) {
-
+        double value = 0;
+        if (x.length != y.length)
+            return value;
+        for (int i = 0; i < x.length; i++) {
+            value += x[i] * y[i];
+        }
+        return value;
     }
+
+    //矩阵和矩阵之积
+    public static double[][] mult(double[][] a, double[][] b) {
+        int m = a.length;
+        int p = a[0].length;
+        int n = b[0].length;
+        if (p != b.length)
+            throw new IllegalArgumentException("file argument is null");
+
+        double[][] result = new double[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = 0;
+                for (int k = 0; k < p; k++)
+                    result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+
+        return result;
+    }
+
+    //转置矩阵
+    public static double[][] transpose(double[][] a) {
+        int m = a.length;
+        int n = a[0].length;
+        double[][] result = new double[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                result[i][j] = a[j][i];
+
+        return result;
+    }
+
+    
 }
